56dab927a927efa104e52cc72a6713d0b4bc76c5
Brandon Hackney <bhackney@uci.edu>
2023-12-28 14:09:24 -0800

    Add function to define paths relative to base folder

---
+++ specifyPaths.m
@@ -0,0 +1,9 @@
+function pths = specifyPaths()
+pths.base = pwd;
+pths.analysis = fullfile(pths.base, 'Analysis');
+% pths.data = fullfile(pths.base, 'ExpOutputs'); % but rename to 'data'
+pths.stimuli = fullfile(pths.base, 'stims'); % verify
+pths.frames = fullfile(pths.base, 'frames'); % verify
+pths.edf = fullfile(pths.base, 'edfImport');
+pths.beh = fullfile(pths.base, 'beh'); % behavioral data e.g. RTs
+end
