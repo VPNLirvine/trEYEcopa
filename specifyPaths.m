a97e309506c715b4b62ac72f2ef158f79df31a60
Brandon Hackney <bhackney@uci.edu>
2024-01-05 15:30:01 -0800

    Update stimulus path locations

--- specifyPaths.m
+++ specifyPaths.m
@@ -4,15 +4,15 @@ pths.analysis = fullfile(pths.base, 'Analysis');
 pths.data = fullfile(pths.base, 'ExpOutputs'); % but rename to 'data'
     pths.MWdat = fullfile(pths.data, 'MWoutput');
     pths.TCdat = fullfile(pths.data, 'TCoutput');
-    pths.fixdat = fullfile(pths.data, 'fixation_checks'); % need to add
+    pths.fixdat = fullfile(pths.data, 'fixation_checks');
 
-pths.stimuli = fullfile(pths.base, 'stims'); % verify
-    pths.MWstim = fullfile(pths.stimuli, 'Martin Weisberg stims'); % verify
+pths.stimuli = fullfile(pths.base, 'stims');
+    pths.MWstim = fullfile(pths.stimuli, 'MartinWeisberg');
+    pths.TCstim = fullfile(pths.stimuli, 'TriCOPA'); % verify contents
     
-    
-pths.frames = fullfile(pths.base, 'frames'); % verify
+pths.frames = fullfile(pths.base, 'frames');
 pths.edf = fullfile(pths.base, 'edfImport');
-pths.edfalt = fullfile(pths.base, 'edf_alt'); % need to add
+pths.edfalt = fullfile(pths.base, 'edf_alt');
 
 pths.beh = fullfile(pths.base, 'beh'); % behavioral data e.g. RTs
 end
