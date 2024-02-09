ab06511fab4d9cf1b0e96b108d10843a43018d56
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2024-02-08 17:07:11 -0800

    Update draft function to get intersubject correlations

--- Analysis/corrTest.m
+++ Analysis/corrTest.m
@@ -1,47 +1,25 @@
-% Assume var data exists
+data = getTCData('ISC');
 
-
-
+subList = unique(data.Subject);
 % Now only analyze correlations based on subject 1
 s = 1;
 s2 = 2;
-numTrials = height(data(s).beh);
+s1dat = data(strcmp(data.Subject, subList{s}), :);
+s2dat = data(strcmp(data.Subject, subList{s2}), :);
+numTrials = height(s1dat);
+ISC = nan([numTrials, 1]);
 for t = 1:numTrials
     % See if this trial exists for subject 2
-    tname = data(s).beh.StimName{t};
-    % Init these vars each time, so you don't have leftover data
-    scanA = [];
-    scanB = [];
-    if ismember(tname, data(s2).beh.StimName)
+    tname = s1dat.StimName{t};
+    if ismember(tname, s2dat.StimName)
         % Figure which trial you need from subject 2
-        ind = find(strcmp(tname, data(s2).beh.StimName));
-        % Slice out the scanpath for both subjects
-        % ...which will take x and y data, right?
-        scanA(:,1) = pickCoordData(data(s).edf(t).Samples.gx);
-        scanA(:,2) = pickCoordData(data(s).edf(t).Samples.gy);
-
-        scanB(:,1) = pickCoordData(data(s2).edf(ind).Samples.gx);
-        scanB(:,2) = pickCoordData(data(s2).edf(ind).Samples.gy);
+        ind = find(strcmp(tname, s2dat.StimName));
+        % Slice out the heatmaps for both subjects
+        heatmap1 = s1dat.Eyetrack{t};
+        heatmap2 = s2dat.Eyetrack{ind};
         
-        % Now you have x,y for every timepoint for both subjects
-        % Determine if either needs to be flipped
-        if data(s).beh.Flipped(t)
-            % Then flip the x data
-            % That requires knowing the screen dimensions...
-            % Uh just hardcode for now I guess: 1920 x 1200
-            screenXMax = 1920;
-            scanA(:,1) = mirrorX(scanA(:,1), screenXMax);
-        end
-        if data(s2).beh.Flipped(ind)
-            % Same
-            screenXMax = 1920;
-            scanB(:,1) = mirrorX(scanB(:,1), screenXMax);
-        end
-        
-        % Now correlate the two x-y vectors somehow
-        % Remembering that they're not necessarily the same length
-        % And also they're both 2-column matrices
-        fprintf(1, '');
+        % Now correlate the heatmaps
+        ISC(t) = corr2(heatmap1, heatmap2);
     else
         % Nothing to compare, so move to next trial
         continue
