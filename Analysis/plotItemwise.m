a87cda54e74efc2e825e2a62db059ecc4b81766b
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2024-05-01 19:42:11 -0700

    Comment in an option to sort the itemwise plot by video duration

--- Analysis/plotItemwise.m
+++ Analysis/plotItemwise.m
@@ -2,9 +2,26 @@ function plotItemwise(plotData, metricName)
 % Generate boxplots showing variability of each stimulus
 % First get some axis labels etc
 [axistxt, yl] = getGraphLabel(metricName);
-plotOrder = unique(plotData.StimName); % Theoretically alphabetical order
+stimList = unique(plotData.StimName); % Theoretically alphabetical order
+
+% % Let's try sorting by video duration instead of video name
+% pths = specifyPaths('..');
+% for i = 1:length(stimList)
+%     stim = stimList{i};
+%     dur = getVideoDuration(fullfile(pths.TCstim, 'normal',stim));
+%     s{i} = stim;
+%     d(i) = dur;
+% end
+% [~,o] = sort(d); % sort the durations and get the reordering indices
+% plotOrder = stimList(o); % use the new order to index from stim list
+% tit = 'Videos sorted by duration, variance is across subjects';
+
+plotOrder = stimList;
+tit = 'Videos sorted alphabetically, variance is across subjects';
+
 % Now make the plot
 figure();
     boxplot(plotData.Eyetrack, plotData.StimName, 'GroupOrder', plotOrder);
     ylim(yl);
     ylabel(axistxt);
+    title(tit);
