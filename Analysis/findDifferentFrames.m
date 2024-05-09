3d9cd7855250c37e798fb557e8ffd9ee7a438154
vpnl <vpnl@dhcp-v104-255.mobile.uci.edu>
2024-05-09 16:30:43 -0700

    Move an analysis function to Analysis folder

--- Analysis/findDifferentFrames.m
+++ Analysis/findDifferentFrames.m
@@ -1,12 +1,23 @@
-function output = findDifferentFrames()
+function output = findDifferentFrames(varargin)
 
-pths = specifyPaths();
+pths = specifyPaths('..');
 vlist = dir(pths.frames);
-vlist(1:3) = [];
+vlist(startsWith({vlist(:).name}, '.')) = []; % drop system folders
 numVids = length(vlist);
 
+if nargin > 0
+    % Input should be a video name
+    % "loop" over just that one video
+    loopRange = find(strcmpi(varargin{1}, {vlist(:).name}));
+    assert(~isempty(loopRange), 'Provided movie name %s not found!', varargin{1});
+    numVids = 1;
+else
+    loopRange = 1:numVids;
+end
+
+fprintf(1, 'Finding first and last frames of motion in videos\n');
 fprintf(1, 'Operating over %i videos\n', numVids);
-for v = 1:numVids
+for v = loopRange
     folderName = vlist(v).name;
     movName = erase(folderName, '.MOV');
     movName = erase(movName, '.mov'); % either case
@@ -18,7 +29,7 @@ for v = 1:numVids
     end
     
     frange = []; % init per vid
-    fprintf(1, '\tVideo %i/%i...', v, numVids);
+    fprintf(1, '\tVideo %i/%i: %s...', v, numVids, movName);
     % Find first frame with motion
     for i = 1:numFrames-1
         fname1 = fullfile(pths.frames, folderName, inOrder{i});
@@ -31,7 +42,7 @@ for v = 1:numVids
         img1 = im2bw(img1);
         img2 = im2bw(img2);
 
-        if ~isequal(img1, img2)
+        if sum(img1 ~= img2, 'all') > 7
             frange(1) = i+1;
             break
         end
@@ -49,12 +60,18 @@ for v = 1:numVids
         img1 = im2bw(img1);
         img2 = im2bw(img2);
 
-        if ~isequal(img1, img2)
+        if sum(img1 ~= img2, 'all') > 7
             frange(2) = i-1;
             break
         end
     end
-    output(v).StimName = movName;
-    output(v).FrameRange = frange;
+    if numVids == 1
+        % 
+        output(1).StimName = movName;
+        output(1).FrameRange = frange;
+    else
+        output(v).StimName = movName;
+        output(v).FrameRange = frange;
+    end
     fprintf(1, 'Done.\n')
 end
