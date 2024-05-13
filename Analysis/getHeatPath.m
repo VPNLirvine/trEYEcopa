5f2664187a020b224816787bbff37dab0933e311
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2024-05-13 14:54:59 -0700

    Set 3D heat'paths' to output histogram details, and avoid plotting by default

--- Analysis/getHeatPath.m
+++ Analysis/getHeatPath.m
@@ -1,10 +1,28 @@
-function getHeatPath(data, stim)
+function output = getHeatPath(data, stim, varargin)
 % If a heat MAP is a 2D representation of single subject's 3D gaze path,
 % then a heat PATH is a 3D representation of ALL subjects' gaze paths.
 % It's like a 3D histogram.
+% This is inherently itemwise, i.e. compares all subjects for one stimulus,
+% thus you ought not correlate this data with subject-level vars like AQ.
 %
-% Input 1 is the data table of gaze paths, e.g. from getTCData('gaze');
+% Input 1 is the full data table of gaze paths, e.g. from getTCData('gaze');
 % Input 2 is the stimulus name as it appears in data.StimName
+% Input 3 (optional) flags whether you want to plot the data or not
+% - flag values: 0 = no plots, 1 = scatter of all, 2 = histogram, 3 = both
+%
+% Output 1 is a struct with two elements:
+% output.Data is the 3D matrix of data representing the bin counts
+% output.Bins is a struct w/ the bin edges: XbinEdges YbinEdges TbinEdges
+% Remember that there are n+1 edges relative to bins:
+% Bin 1 covers the range edge(1):edge(2), ... bin N is edge(N):edge(N+1)
+
+% Parse varargin
+if nargin > 2
+    flag = varargin{1};
+else
+    % No plots by default
+    flag = 0;
+end
 
 % Find the index of that stim name
 stimList = unique(data.StimName);
@@ -19,12 +37,14 @@ numSub = length(subList);
 
 % As an intermediary step, plot all those paths stacked on top of each other
 % but turn this section off if you're looping over stimulus
-figure();
-hold on
-for i = 1:length(pathList)
-    plotGaze(pathList{i}, stim);
+if flag == 1 || flag == 3
+    figure();
+    hold on
+    for i = 1:length(pathList)
+        plotGaze(pathList{i}, stim);
+    end
+    hold off
 end
-hold off
 
 % Now reorganize the data in prep for making a histogram:
 % write each element of each gaze vector into a single row of one giant table
@@ -54,5 +74,6 @@ for i = 1:numSub
 end
 
 % Finally, run it through a 3D histogram and plot again.
-plotDensity(tbl, stim);
+flag2 = flag >= 2; % Determine whether to plot or not
+[output.Data, output.Bins] = plotDensity(tbl, stim, flag2); % Get data, maybe plot too
 fprintf(1, 'Done.\n')
--- Analysis/plotDensity.m
+++ Analysis/plotDensity.m
@@ -1,5 +1,18 @@
-function plotDensity(tbl, stim)
+function [output, varargout] = plotDensity(tbl, stim, varargin)
 % Adapted from a user named 'Hoki' on StackOverflow
+% given some XYZ data for multiple subjects, generate a 3D histogram
+% Bin sizes are hardcoded within, which is obviously less than ideal
+
+% Parse inputs
+if nargin > 2
+    % Allow bypassing the plot to just get the data
+    flag = varargin{1};
+else
+    % Plot by default, since it's called "plot density"
+    flag = true;
+end
+
+% Define constants
 x = tbl.x;
 y = tbl.y;
 z = tbl.time;
@@ -30,6 +43,7 @@ npt = sum(Nz1, 'all'); % we may be excluding outliers like blinks here
 % preallocate 3D containers
 N3d = zeros(nybins,nxbins,ntbins) ; % 3D matrix containing the counts
 Npc = zeros(nybins,nxbins,ntbins) ; % 3D matrix containing the percentages
+Nsub = zeros(nybins,nxbins,ntbins); % 3D matrix containing subject agreemnt
 colorpc = zeros(npt,1) ;         % 1D vector containing the percentages
 pctsubs = zeros(npt,1) ;
 
@@ -71,8 +85,9 @@ for k=1:nv
         idz   = find( zbins==vzbins(kz) ) ;
         idx3d = idx(idz) ;
         colorpc(idx3d) = thisColorpc ;
-        numUsedSubs = length(unique(tbl.Subject(idx)));
+        numUsedSubs = length(unique(tbl.Subject(idx3d)));
         pctsubs(idx3d) = numUsedSubs / numSubs;
+        Nsub(ybin,xbin,kz) = numUsedSubs / numSubs;
     end
 
 end
@@ -84,35 +99,49 @@ assert(isequal(chk,Nz1), 'Encountered an unexpected binning error after passing 
 numCounted = sum(N3d, 'all');
 assert(  numCounted == npt, 'Only considered %i of %i timepoints!', numCounted, npt ) % double check we counted everything
 
+%% Define function outputs
+% Main output should give the bin counts
+output = Nsub;
+
+% Second output gives you the bin widths/edges
+if nargout > 1
+    Bins.XbinEdges = xBins;
+    Bins.YbinEdges = yBins;
+    Bins.TbinEdges = tBins;
+    varargout{1} = Bins;
+end
 %% Display final result
 dotSize = (colorpc + eps) ./ max(colorpc + eps); % rescale to percentage
 % dotColor = dotSize;
 dotColor = pctsubs;
 % Apply a threshold on which dots to plot,
 % otherwise it plots literally every sample for every subject.
-dropThese = dotSize < 0.1; % apply a threshold
-    x(dropThese) = [];
-    z(dropThese) = [];
-    y(dropThese) = [];
-    dotSize(dropThese) = [];
-    dotColor(dropThese) = [];
+% dropThese = dotSize < 0.1; % apply a threshold
+%     x(dropThese) = [];
+%     z(dropThese) = [];
+%     y(dropThese) = [];
+%     dotSize(dropThese) = [];
+%     dotColor(dropThese) = [];
 dotSize = dotSize .* binRes; % resize based on bin size
 % Now draw the plot
-h=figure;
-hs=scatter3(x, z, y, dotSize , dotColor ,'filled' );
-xlabel('x');
-ylabel('Time in ms');
-zlabel('y');
-title(replace(stim,'_','\_'));
-xlim([0 xMax]);
-zlim([0 yMax]);
-% Avoid using scientific notation for the time axis
-ax = gca; % axes handle
-ax.YAxis.Exponent = 0;
-% Fix axis directions
-set(ax, 'YDir', 'reverse'); % time goes from back to front
-set(ax, 'ZDir', 'reverse');
-% Set colorbar
-cb = colorbar ;
-cb.Label.String = 'Percentage of subjects with data in this bin';
-% cb.Label.String = 'Bin density';
+if flag
+    h=figure;
+    hs=scatter3(x, z, y, dotSize , dotColor ,'filled' );
+    xlabel('x');
+    ylabel('Time in ms');
+    zlabel('y');
+    title(replace(stim,'_','\_'));
+    xlim([0 xMax]);
+    zlim([0 yMax]);
+    % Avoid using scientific notation for the time axis
+    ax = gca; % axes handle
+    ax.YAxis.Exponent = 0;
+    % Fix axis directions
+    set(ax, 'YDir', 'reverse'); % time goes from back to front
+    set(ax, 'ZDir', 'reverse');
+    % Set colorbar
+    cb = colorbar ;
+    cb.Label.String = 'Percentage of subjects with data in this bin';
+    clim([0 1]); % This is a percentage
+    % cb.Label.String = 'Bin density';
+end
