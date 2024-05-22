8a8978b65f5b66d926bb893f00576a5677594734
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2024-05-21 18:36:52 -0700

    Write function to compare gaze to ROIs

---
+++ Analysis/addframe2gaze.m
@@ -0,0 +1,46 @@
+function gaze = addframe2gaze(edfDat, i)
+% Given a single row of EDF data (i.e. one trial),
+% and the row index of the eye to use (determined by selectMetric),
+% export the XY gaze path WITH the number of the video frame that was up
+
+
+% Search the Events struct to find messages defining frame changes
+list = edfDat.Events.message;
+stxt = 'Frame to be displayed ';
+y = cellfun(@(x) contains(x,stxt), list);
+assert(sum(y) > 0, 'No message re frame timing found!');
+% Get the frame numbers associated with each true element in y
+frames = list(y);
+frameNums = cellfun(@(x) str2double(erase(x,stxt)), frames);
+% Use those indices to extract the timestamps of frame updates
+frameTimes = edfDat.Events.sttime(y);
+% Append the final frame's offset time, so you know when to stop
+% Defined as the time the video is removed from screen
+    y2 = cellfun(@(x) contains(x, 'BLANK_SCREEN'), list);
+    assert(sum(y2) > 0, 'No message re video end found!');
+    frameTimes(end+1) = edfDat.Events.sttime(y2);
+% Use those timestamps to compare to Sample timestamps
+sampleTimes = edfDat.Samples.time;
+gaze = NaN([3,numel(edfDat.Samples.time)]); % init to a larger size than needed, then shrink later
+c = 1;
+for f = 1:numel(frameTimes) - 1
+    % Which samples fit the bill?
+    y3 = sampleTimes >= frameTimes(f) & sampleTimes < frameTimes(f+1);
+    % How many are there?
+    fend = sum(y3);
+    npos = c+fend-1;
+    % Get X
+    gaze(1,c:npos) = edfDat.Samples.gx(i,y3);
+    % Get Y
+    gaze(2,c:npos) = edfDat.Samples.gy(i,y3);
+    % Get frame number
+    gaze(3, c:npos) = frameNums(f); % probably same as f
+    % increment
+    c = npos+1;
+end
+% Strip out the unused columns
+gaze(:,isnan(gaze(1,:))) = [];
+% Now we have every X and Y gaze coordinate during the video, plus frame #
+
+
+end
--- Analysis/timeOnTarget.m
+++ Analysis/timeOnTarget.m
@@ -11,12 +11,27 @@ trialNum = 5; % say
 edf = osfImport('TC_19.edf');
 edfDat = edf(trialNum);
 
+% A flag for whether the video was flipped on this trial or not
+% Comes as a varargin to selectMetric
+flipFlag = true;
+
+% selectMetric determines which eye to use
+i = 2; % for now
+
     % We can extract the stim name from that
     % ...but it may have a path attached that we should remove
     stimName = getStimName(edfDat);
 
 % The 'window' vector defining the location of the video on screen
-pos = [xLeft xRight yTop yBottom];
+% pos = [xLeft xRight yTop yBottom];
+wRect = [0 0 1920 1200]; % the shape of the stim monitor
+Movx = 674;
+Movy = 504;
+% Be aware that the videos we use in the experiment
+% are a slightly different size than the ones I have locally:
+% exp is 674 x 504, new is 676 x 506
+% But since we USED the smaller ones, let's use those values here:
+pos = resizeVideo(Movx, Movy, wRect);
 % Get the position data while rescaling it to fit the display area
 % Be mindful this imports all position data first, then subsets,
 % so you're wasting compute if you do this inside a loop.
@@ -32,20 +47,32 @@ posDat = resizePosition(stimName, pos);
 % e.g. if the video is at 60 fps but the eyetracker is at 250 Hz,
 % then there are 250/60 gaze samples per video frame,
 % which is non-integer 4.167
-% AH EXCEPT the EDF file SHOULD have messages for frame number!
-
-% edfDat.Events.message has the data
-% 0 !V VFRAME fnum leftXPos topYPos filename
-% this has the XY coordinates of the top left corner
-% or just look for 'Frame to be displayed X' which comes immediately before
-% Both are displayed immediately after the frame goes up
-
-% Search the Events struct to find messages defining frame changes
-% Use those indices to extract the timestamps of frame updates
-% Use those timestamps to compare to Sample timestamps
-% Apply a label to each Sample describing which frame was displayed
-% ...unless that's existing data??
-
+% so it's not as simple as just e.g. playing each frame twice
+% Need to know what frame was up for each gaze sample,
+% then resample the position data to follow that pattern.
+gaze = addframe2gaze(edfDat, i);
+% We also need to un-flip the gaze for flipped videos
+if flipFlag
+    gaze(1,:) = mirrorX(gaze(1,:), wRect(3));
+end
+% Row 3 is the frame number. Use that to index out of posDat.
+C1pos = [ posDat(1).X(gaze(3,:)) ; posDat(1).Y(gaze(3,:)) ];
+C2pos = [ posDat(2).X(gaze(3,:)) ; posDat(2).Y(gaze(3,:)) ];
+C3pos = [ posDat(3).X(gaze(3,:)) ; posDat(3).Y(gaze(3,:)) ];
+C4pos = [ posDat(4).X(gaze(3,:)) ; posDat(4).Y(gaze(3,:)) ];
 
 %% COMPARE GAZE AND POSITION
-% Define a radius around each character 
+% Define a radius around each character
+rad = 200; % for now - need to know character size on screen
+
+% Define logicals to indicate whether gaze is near each character
+gazeOnC1 = gaze(1,:) >= C1pos(1,:) - rad & gaze(1,:) <= C1pos(1,:) + rad & gaze(2,:) >= C1pos(2,:) - rad & gaze(2,:) <= C1pos(2,:) + rad;
+gazeOnC2 = gaze(1,:) >= C2pos(1,:) - rad & gaze(1,:) <= C2pos(1,:) + rad & gaze(2,:) >= C2pos(2,:) - rad & gaze(2,:) <= C2pos(2,:) + rad;
+gazeOnC3 = gaze(1,:) >= C3pos(1,:) - rad & gaze(1,:) <= C3pos(1,:) + rad & gaze(2,:) >= C3pos(2,:) - rad & gaze(2,:) <= C3pos(2,:) + rad;
+gazeOnC4 = gaze(1,:) >= C4pos(1,:) - rad & gaze(1,:) <= C4pos(1,:) + rad & gaze(2,:) >= C4pos(2,:) - rad & gaze(2,:) <= C4pos(2,:) + rad;
+
+% From here, you can do multiple things,
+% like calculate the time spent on one specific character,
+% or tally the total number of alternations between any characters,
+% or 'triangle time' i.e. proportion of time on any character,
+% etc.
