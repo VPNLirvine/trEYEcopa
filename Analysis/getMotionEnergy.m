bf52772ee442d4fee1ef7a33e2365ada29e54ae8
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2024-08-22 14:58:49 -0700

    Get motion data for all TriCOPA stims

--- Analysis/getMotionEnergy.m
+++ Analysis/getMotionEnergy.m
@@ -1,26 +1,26 @@
-function output = getMotionEnergy(vidPath)
-% For a given video, estimate the amount of motion per frame
-% Rather than using complicated methods with e.g. banks of Gabor filters,
-% here we just report the average intensity difference per frame.
+function motion = getMotionEnergy()
+% Point this function at a stimulus folder
+% Loop over every file in the folder
+% Calculate motion energy vectors, then store them as a .mat
 
-% Load the video
-thisVid = VideoReader(vidPath);
-numFrames = thisVid.NumFrames;
+pths = specifyPaths('..');
+stimDir = fullfile(pths.TCstim, 'normal');
+stimList = dir(fullfile(stimDir, '*.mov')); % ignore any CSVs in there
+numStims = length(stimList);
 
-globSize = ceil(thisVid.FrameRate); % read many frames at once to improve performance
+motion = table('Size', [numStims, 2], 'VariableTypes', {'string', 'cell'}, 'VariableNames', {'StimName', 'MotionEnergy'}); % init
 
-output = zeros([numFrames, 1]); % column
+fprintf(1, 'Getting motion data for %i videos.\n', numStims)
+for s = 1:numStims
+    fprintf(1, '\t%i\n', s);
+    stimName = stimList(s).name;
+    fname = fullfile(stimDir, stimName);
+    motion = getMotionEnergy(fname);
 
-% Read in a glob of frames, then compare each j to j+1
-for g = 1:globSize:numFrames-1
-    fs = read(thisVid, [g, min([numFrames, g + globSize])]);
-    for j = 1:size(fs,4)-1
-        i = g + j - 1; % frame number
-        img1 = fs(:,:,:,j);
-        img2 = fs(:,:,:,j+1);
-        
-        % Simple comparison
-        output(i+1) = mean(abs(img2 - img1), 'all');
-    end
+    motion.StimName{s} = stimName;
+    motion.MotionEnergy{s} = motion;
 end
-end
+fprintf(1, 'Done.\n');
+
+% Save to disk, since this takes a minute or two to load
+save('motionData.mat', 'motion');
