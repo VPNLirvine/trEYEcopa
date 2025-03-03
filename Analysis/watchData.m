bcb2000a7fc278b0f4191c65b5134a6b75e30286
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2025-03-03 15:24:59 -0800

    Adapt watchData to map-based deviance data

--- Analysis/watchData.m
+++ Analysis/watchData.m
@@ -5,9 +5,17 @@ function watchData(movName, gaze, prediction)
     % Input 2 is a 4*n gaze vector that must include frame indices
     % (Optional) Input 3 is a prediction vector of similar size
     
-    if nargin > 2 && islogical(prediction)
+    watchPred = false;
+    if nargin > 2 && islogical(prediction) && prediction
+        watchPred = true;
         % Optionally allow a logical that fetches the proper gaze
-        prediction = motionDeviation(gaze, movName);
+        prediction = motionDeviation(movName);
+        % Convert to a mask
+        for i = 1:size(prediction, 3)
+            % Inside a loop bc otherwise it's 3D distance
+            prediction(:,:,i) = bwdist(prediction(:,:,i));
+        end
+        prediction = prediction <= deg2pix(3);
     end
 
     numSamples = width(gaze);
@@ -18,14 +26,15 @@ function watchData(movName, gaze, prediction)
     [~,movName,~] = fileparts(movFName); % ensure no extension attached
     fprintf(1, 'Loading frames...');
     thisVid = VideoReader(movFName);
-    frames = read(thisVid);
+    frame = readFrame(thisVid);
     fprintf(1, 'Done.\n');
 
     % Get the dimensions of the video
     imh = thisVid.Height;
     imw = thisVid.Width;
-    numFrames = size(frames,4);
-    clear thisVid % release memory
+    % numFrames = size(frames,4);
+    numFrames = thisVid.NumFrames;
+    % clear thisVid % release memory
 
 %     scVec = get(0, 'ScreenSize');  % Get the screen size
     scVec = [1,1,1920,1200]; % size of stimulus monitor
@@ -44,33 +53,48 @@ function watchData(movName, gaze, prediction)
         ax.Units = 'pixels';  % Set the units of the axes to pixels
         i = 1;
         title(movName);
-        h0 = image([pos(1), pos(3)], [pos(2), pos(4)], frames(:,:,:,i));
+        h0 = image([pos(1), pos(3)], [pos(2), pos(4)], frame);
         hold on;
-        h1 = scatter(gaze(1,i), gaze(2,i), (10*.7)^2, 'o', 'MarkerFaceColor', 'r');
-        if nargin > 2
-            h2 = scatter(prediction(1,i), prediction(2,i), (deg2pix(3) * .7)^2, 'o', 'MarkerFaceColor', 'b');
+        if watchPred
+            green = cat(3, zeros(size(prediction(:,:,1))), ones(size(prediction(:,:,1))), zeros(size(prediction(:,:,1))));
+            h2 = image(green);
+            set(h2, 'AlphaData', prediction(:,:,i));
+            % h2 = scatter(prediction(1,i), prediction(2,i), (deg2pix(3) * .7)^2, 'o', 'MarkerFaceColor', 'b');
+            % h2.MarkerFaceAlpha = .2;
         end
-        h2.MarkerFaceAlpha = .2;
+        h1 = scatter(gaze(1,i), gaze(2,i), (10*.7)^2, 'o', 'MarkerFaceColor', 'r');
         hold off;
     titxt = strrep(movName, '_', '\_');
     % Now animate in a new loop
-    tic
+    t = tic;
+    lastFrame = i;
     for i = 2:numSamples
         f = frameIdx(i);
-        h0.CData = frames(:,:,:,f);
+        if lastFrame < f
+            h0.CData = readFrame(thisVid);
+            lastFrame = lastFrame + 1;
+        end
+        % h0.CData = frames(:,:,:,f);
+        % h0.CData = readFrame(thisVid);
         h1.XData = gaze(1,i);
         h1.YData = gaze(2,i);
-        if nargin > 2
-            h2.XData = prediction(1,i);
-            h2.YData = prediction(2,i);
+        if watchPred
+            % h2.XData = prediction(1,i);
+            % h2.YData = prediction(2,i);
+            set(h2, 'AlphaData', prediction(:,:,f) .* .5);
         end
         title(titxt);
-        % drawnow;
-        % pause(1/15); % compensate for frame rate
-        pause(sr);
+        
+        % Compensate for framerate
+        if toc > sr
+            drawnow;
+            tic;
+        end
+        % drawnow limitrate;
+        % pause(sr);
     end
     
-    toc  % Display the elapsed time
+    toc(t);  % Display the elapsed time
     close all  % Close all figures
     clear frames % in case it doesn't automatically
 
