8c0d2cf6f0cc3781e7b8c68869ad62f3e5ccd908
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2025-07-10 20:08:45 -0700

    Add QC metric 'interp'

--- Analysis/censorBlinks.m
+++ Analysis/censorBlinks.m
@@ -1,4 +1,4 @@
-function output = censorBlinks(output, edfDat)
+function [output, bs] = censorBlinks(output, edfDat)
 % Reject blink data and interpolate replacement values
 % But the default blink detection is slightly too conservative,
 % so pad the given values to catch on/off artifacts
@@ -15,5 +15,13 @@ if isfield(edfDat, 'Blinks') && ~isempty(edfDat.Blinks)
     bs = ismember(gazeTimes, blinkDurs); % blink samples
     % Now interpolate the values for bs based on everything else
     output(:,bs) = interp1(gazeTimes(~bs), output(~bs), gazeTimes(bs), 'makima');
+
+    % but don't output the number with the buffer
+
+else
+    % 'output' is the input, so don't need to define it
+    % but if no blinks were found, return an empty vector
+    bs = [];
 end
+
 end
